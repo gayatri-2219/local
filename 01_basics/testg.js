341faae7cdbcfe7926015b15e9a6761c61388538
gayatri-2219 <gayatrilearning2219@gmail.com>
2025-07-14 16:08:05 +0530

    added more information

--- 01_basics/testg.js
+++ 01_basics/testg.js
@@ -4,3 +4,17 @@ const accountId = 1234
 let accountEmail = "gayatri@gmail.com" 
 var accountPass= "1233456"
 accountCity= "Maharstra"
+let accountState;  //undefined
+//accountId= 3
+console.log(accountId)
+
+console.table([accountId,accountEmail, accountCity,accountState])
+/*Prefer not to use var
+due to issue in block scope and functional scope
+ */
+
+
+//DATATYPES
+"use strict"; // treat all js code newer version
+
+//alert("hello") // this will work on inspect browser console not in new version
